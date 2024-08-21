db370b1b75e5921d00fc728abb93574b2453a82b
Utkarsh Saxena <utkarsh2807saxena@gmail.com>
2024-08-22 01:40:43 +0530

    Added Project

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,14 +1,11 @@
 import styles from './ProjectsStyles.module.css';
-// import connect4 from '@/assets/connect4game.png';
-// import todolist from '@/assets/todolist.png';
-// import ecommerce from '@/assets/ecommerce.png';
-// import calculator from '@/assets/calculator.png';
 import ProjectCard from '@/common/ProjectCard';
 import { FaCalculator } from 'react-icons/fa';
 import { IoGameController } from 'react-icons/io5';
 import { FaClipboardList } from 'react-icons/fa';
 import { IoBagHandle } from 'react-icons/io5';
 import { PiArticleMediumFill } from 'react-icons/pi';
+import { MdOutlineMarkUnreadChatAlt } from 'react-icons/md';
 
 function Projects() {
   return (
@@ -16,35 +13,36 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          // src={connect4}
           Icon={PiArticleMediumFill}
           link="https://github.com/Utkarsh020470/insights_hub_client"
           h3="Insights Hub"
           p="Blog Website"
         />
         <ProjectCard
-          // src={connect4}
+          Icon={MdOutlineMarkUnreadChatAlt}
+          link="https://github.com/Utkarsh020470/chat_space"
+          h3="Chat Space"
+          p="Chat Website"
+        />
+        <ProjectCard
           Icon={IoGameController}
           link="https://github.com/Utkarsh020470/CONNECT-4-GAME"
           h3="Connect-4"
           p="Game"
         />
         <ProjectCard
-          // src={todolist}
           Icon={FaClipboardList}
           link="https://github.com/Utkarsh020470/TO-DO-LIST"
           h3="To-Do List"
           p="Task Manager"
         />
         <ProjectCard
-          // src={ecommerce}
           Icon={IoBagHandle}
           link="https://github.com/Utkarsh020470/E-COMMERCE-CLONE"
           h3="E-Commerce"
           p="Clone Website"
         />
         <ProjectCard
-          // src={calculator}
           Icon={FaCalculator}
           link="https://github.com/Utkarsh020470/Calculator"
           h3="Calculator"
